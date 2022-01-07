5be40a24f3226323e57c2bf6f32dfe3651f2567e
Fatemeh Brizendine <ssaberi324@yahoo.com>
2022-01-06 20:00:42 -0800

    update index.js in models folder

--- models/index.js
+++ models/index.js
@@ -17,14 +17,14 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: '',
-  foreignKey: ''
+  as: 'tag_id',
+  foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: '',
+  as: 'product_id',
   foreignKey: 'tag_id'
 });
 
