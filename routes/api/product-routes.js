e38e8e663610cdddac347f5996f1fa55dec6cf8b
Fatemeh Brizendine <ssaberi324@yahoo.com>
2022-01-08 21:47:44 -0800

    update product-routes.js to add routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,37 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      include: [
+        Category,
+        Tag
+      ]
+    })
+    res.json(products)
+  } catch(error) {
+    res.status(500).send(error)
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        Category,
+        Tag
+      ]
+    })
+    res.json(products)
+  } catch(error) {
+    res.status(500).send(error)
+  }
 });
 
 // create new product
@@ -91,6 +116,16 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const products = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.json(products)
+  } catch(error) {
+    res.status(500).send(error)
+  }
 });
 
 module.exports = router;
